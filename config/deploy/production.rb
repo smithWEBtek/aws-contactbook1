fdab081fdff8d140bceb858c4045ac4edf3fd8ce
smithwebtek <brad@smithwebtek.com>
2017-03-08 16:04:56 -0500

    update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,10 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+# ec2-52-90-143-7
+server 'ec2-52-90-143-7', user: 'deploy', roles: %w{web app db}
+
+
+
+
