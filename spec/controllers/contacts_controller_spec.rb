5ab38766f6aabdad6c848e4503f470d615366514
smithwebtek <brad@smithwebtek.com>
2017-09-16 11:53:03 -0400

    add rspec tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
-  describe "GET index" do
+	
+	let(:attributes) do
+		{
+			name: "Dusty",
+			email: "dusty@abc.com",
+			phone: "212-413-2411",
+			address: "24 Main St., Boise, ID"
+		}
+	end
+	
+	describe "GET index" do
 		it "assigns @contacts" do
 			contact1 = Contact.create!(name: "Andy", email: "andy@abc.com")
 			contact2 = Contact.create!(name: "Mandy", email: "mandy@abc.com")
@@ -16,6 +26,25 @@ RSpec.describe ContactsController, type: :controller do
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
+    end
+  end
+
+  it "renders the show template" do
+    contact = Contact.create!(attributes)
+    get :show, id: contact.id
+    expect(response).to render_template(:show)
+  end
+
+  describe "creation" do
+    before { post :create, contact: attributes }
+    let(:contact) { Contact.find_by(name: "Dusty") }
+ 
+    it "creates a new contact" do
+      expect(contact).to_not be_nil
+    end
+ 
+    it "redirects to the contact's show page" do
+      expect(response).to redirect_to(contact_path(contact))
     end
   end
 end
